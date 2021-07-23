cd70511de7a5bcc512822efe0cecfca474e015e6
David <dreko@calpoly.edu>
2021-07-23 13:26:06 -0600

    initial commit

---
+++ src/components/Content.js
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { CenterFocusStrong } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+    content: {
+        textAlign: 'center',
+    }
+  }));
+
+function Content() {  
+    const classes = useStyles();
+
+    return (
+      <div className={classes.content}>
+          <h1>This will be my personal website!</h1>
+          <h2>Still a work in progress!</h2>
+          <h3>Coming soon!</h3>
+      </div>
+    );
+  }
+  
+  export default Content;
